c0af4e84caa4a67109ac4b03a44aa04e759cc036
poga07 <chris@168money.com.tw>
2025-01-14 13:11:04 +0800

    Initial Commit

---
+++ JayChouAlbum-SwiftUI/JayChouAlbum_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  JayChouAlbum_SwiftUIApp.swift
+//  JayChouAlbum-SwiftUI
+//
+//  Created by 黃柏嘉 on 2025/01/14.
+//
+
+import SwiftUI
+
+@main
+struct JayChouAlbum_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
