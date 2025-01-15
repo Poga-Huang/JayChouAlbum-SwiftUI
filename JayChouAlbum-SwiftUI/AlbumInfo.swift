9e1c4dadca7139e03c581030b681bef13b344bd0
poga07 <chris@168money.com.tw>
2025-01-15 17:15:59 +0800

    ....

--- JayChouAlbum-SwiftUI/AlbumInfo.swift
+++ JayChouAlbum-SwiftUI/AlbumInfo.swift
@@ -6,3 +6,74 @@
 //
 
 import Foundation
+
+struct AlbumInfo {
+    
+    var index: String
+    var title: String
+    var releaseDate: String
+    var count: Int
+    
+    static let infos: [AlbumInfo] = [
+        AlbumInfo(index: "1",
+                  title: "Jay",
+                  releaseDate: "2000/11/07",
+                  count: 10),
+        AlbumInfo(index: "2",
+                  title: "范特西",
+                  releaseDate: "2001/09/14",
+                  count: 10),
+        AlbumInfo(index: "3",
+                  title: "八度空間",
+                  releaseDate: "2002/07/18",
+                  count: 10),
+        AlbumInfo(index: "4",
+                  title: "葉惠美",
+                  releaseDate: "2003/07/31",
+                  count: 11),
+        AlbumInfo(index: "5",
+                  title: "七里香",
+                  releaseDate: "2004/08/03",
+                  count: 10),
+        AlbumInfo(index: "6",
+                  title: "11月的蕭邦",
+                  releaseDate: "2005/11/01",
+                  count: 12),
+        AlbumInfo(index: "7",
+                  title: "依然范特西",
+                  releaseDate: "2006/09/05",
+                  count: 11),
+        AlbumInfo(index: "8",
+                  title: "我很忙",
+                  releaseDate: "2007/11/02",
+                  count: 10),
+        AlbumInfo(index: "9",
+                  title: "魔杰座",
+                  releaseDate: "2008/10/14",
+                  count: 11),
+        AlbumInfo(index: "10",
+                  title: "跨時代",
+                  releaseDate: "2010/05/18",
+                  count: 11),
+        AlbumInfo(index: "11",
+                  title: "驚嘆號",
+                  releaseDate: "2011/11/11",
+                  count: 11),
+        AlbumInfo(index: "12",
+                  title: "12新作",
+                  releaseDate: "2012/12/28",
+                  count: 12),
+        AlbumInfo(index: "13",
+                  title: "哎呦，不錯哦",
+                  releaseDate: "2014/12/26",
+                  count: 12),
+        AlbumInfo(index: "14",
+                  title: "周杰倫的床邊故事",
+                  releaseDate: "2016/06/24",
+                  count: 10),
+        AlbumInfo(index: "15",
+                  title: "最偉大的作品",
+                  releaseDate: "2022/07/15",
+                  count: 12)
+    ]
+}
--- JayChouAlbum-SwiftUI/ContentView.swift
+++ JayChouAlbum-SwiftUI/ContentView.swift
@@ -10,12 +10,99 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            HeaderView()
+            
+            AlbumsView()
+        
+            Spacer()
+        }
+        .background(.white)
+        .foregroundStyle(.black)
+    }
+}
+
+struct HeaderView: View {
+    var body: some View {
+        HStack {
+            VStack(alignment: .leading) {
+                Text("周杰倫 Jay Chou")
+                    .font(.title3)
+                
+                Text("歷年專輯列表 ")
+                    .font(.system(.title,
+                                  design: .rounded,
+                                  weight: .black))
+            }
+            
+            Spacer()
         }
         .padding()
+    }
+}
+
+struct AlbumsView: View {
+    var body: some View {
+        ScrollView(.horizontal, showsIndicators: false) {
+            HStack {
+                ForEach(AlbumInfo.infos, id: \.title) { info in
+                    AlbumView(info: info)
+                }
+            }
+        }
+
+    }
+}
+
+struct AlbumView: View {
+    
+    var info: AlbumInfo
+    
+    var body: some View {
+        VStack {
+            ZStack(alignment: .bottomLeading){
+                Image(info.title, bundle: nil)
+                    .resizable()
+                    .frame(maxWidth: 200, maxHeight: 200)
+                    .aspectRatio(contentMode: .fill)
+                
+                Text(info.index)
+                    .frame(width: 30, height: 30)
+                    .background(.black)
+                    .foregroundStyle(.white)
+                    .font(.system(size: 20,
+                                  weight: .bold,
+                                  design: .rounded))
+            }
+                
+            HStack {
+                VStack(alignment: .leading, spacing: 4) {
+                    Text(info.title)
+                        .font(.system(.title2,
+                                      design: .rounded,
+                                      weight: .bold))
+                    
+                    Text("發行日期: \(info.releaseDate)")
+                        .font(.subheadline)
+                    
+                    Text("歌曲總數: \(info.count)首")
+                        .font(.footnote)
+                }.padding()
+                
+                Spacer()
+            }
+        }
+        .frame(maxWidth: .infinity, maxHeight: 300)
+        .clipShape(.rect(cornerRadius: 10))
+        .overlay(content: {
+            RoundedRectangle(cornerRadius: 10)
+                .stroke(Color(white: 0.1, opacity: 0.1), lineWidth: 1)
+                .shadow(color: .black, radius: 2,x: 1, y: 1)
+        })
+        .rotation3DEffect(
+            .degrees(20),
+            axis: (x: 0.0, y: 1.0, z: 0.0)
+        )
+        .padding(20)
     }
 }
 
